70c6427206e6e6525d3d0585c43f24e78cbcfb39
igor <cruigor@gmail.com>
2026-04-28 12:05:16 -0300

    feat: criando pasta de simulations

--- cnc/gcode_generator/main.c
+++ cnc/gcode_generator/main.c
@@ -4,13 +4,13 @@
 int main() {
     init_gcode();
 
-    for (int row = 0; row < 8; row++) {
+    for (int row = 0; row < 4; row++) {
         if (row % 2 == 0) {
-            for (int col = 0; col < 12; col++) {
+            for (int col = 0; col < 6; col++) {
                 process_well(row, col);
             }
         } else {
-            for (int col = 11; col >= 0; col--) {
+            for (int col = 5; col >= 0; col--) {
                 process_well(row, col);
             }
         }
